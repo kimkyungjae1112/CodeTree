7707ba670b036006178d12c971b2b3f792decd69
김경재 <74956602+kimkyungjae1112@users.noreply.github.com>
2025-10-23 09:25:31 +0900

    Title: 가장 짧은 부분합, Time: 29ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 251023/가장 짧은 부분합/shortest-subtotal.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+using namespace std;
+
+int N, S;
+
+int main() 
+{
+    cin >> N >> S;
+
+    vector<int> vec(N);
+    for(int i = 0; i < N; ++i)
+    {
+        cin >> vec[i];
+    }
+
+    int Sum = vec[0];
+    int j = 0;
+    int Answer = numeric_limits<int>::max();
+
+    for(int i = 0; i < N; ++i)
+    {
+        while(j + 1 < N && Sum < S)
+        {
+            Sum += vec[j + 1];
+            j++;
+        }
+
+        if(Sum >= S)
+        {
+            Answer = min(Answer, j - i + 1);
+        }
+
+        Sum -= vec[i];
+
+    }   
+
+    cout << Answer;
+    return 0;
+}
