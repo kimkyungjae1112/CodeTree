26d96985fb3a2a2af84baffa1b7edfde7a7e65b9
김경재 <74956602+kimkyungjae1112@users.noreply.github.com>
2025-10-27 08:14:37 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 12ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251027/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+const int MIN = numeric_limits<int>::min();
+int DP[100001];
+int A[100001];
+int N;
+
+int main() 
+{
+    cin >> N;
+    for(int i = 0; i < N; ++i)
+    {
+        cin >> A[i];
+    }
+
+    for(int i = 0; i < N; ++i)
+    {
+        DP[i] = MIN;
+    }
+    DP[0] = A[0];
+
+    for(int i = 1; i < N; ++i)
+    {
+        DP[i] = max(A[i], DP[i-1] + A[i]);
+    }
+
+    int Max = -1001;
+    for(int i = 0; i < N; ++i)
+    {
+        Max = max(Max, DP[i]);
+    }
+    cout << Max;
+
+    return 0;
+}
